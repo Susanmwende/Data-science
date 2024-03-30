2493d8c15452e9154a199534df4b30f9e08fc913
Susanmwende <mwendezmasaku@gmail.com>
2024-03-31 00:30:44 +0300

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# adding items to a tuple
+city=("Delhi","Nairobi","Mombasa","Kisumu")
+newcity = list(city)
+newcity[1]="Addis Ababa"
+city= tuple(newcity)
+print(city)
+
+# Add 
+citi=("Delhi","Nairobi","Mombasa","Kisumu")
+newcities = citi + ("Addis ababa",)
+print(newcities)
+
+
+#Remove
+city=("Delhi","Nairobi","Mombasa","Kisumu")
+newcity = list(city)
+newcity.remove("Mombasa")
+city= tuple(newcity)
+print(city)
+
+
+
+
+
+
