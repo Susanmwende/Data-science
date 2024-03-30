e026e1252fadd592e3401eed27293d39ff17a68d
Susanmwende <mwendezmasaku@gmail.com>
2024-03-31 00:24:40 +0300

    stack.py

---
+++ stack.py
@@ -0,0 +1,16 @@
+stack = []
+stack.append("Susan")
+stack.append("mwende")
+stack.append("Masaku")
+print(stack)
+
+stack = ['Susan', 'mwende', 'Masaku']
+print(stack.pop())
+print(stack.pop())
+
+stack =  ['Susan', 'mwende', 'Masaku']
+print(stack[1])
+print(stack[2])
+
+
+
